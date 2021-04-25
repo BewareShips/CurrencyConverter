f45d737723e70eb436201b72f123c00f52b89d7c
Edem Nedipov <edenpolovets@gmail.com>
2021-04-25 12:07:56 +0300

    works with inputs

--- src/components/CurrencyRow.tsx
+++ src/components/CurrencyRow.tsx
@@ -1,22 +1,27 @@
-import React from 'react'
+import events from "events";
+import React from "react";
 
 export interface CurOptions {
-  currencyOptions: any[]
+   currencyOptions: any[];
+   selectedCurrency: string ;
+   onChangeCurrency:(e:any)=>void;
+   amount?: any;
+   setAmount?:(e:any)=>void;
 }
 
-const CurrencyRow = ({currencyOptions}:CurOptions) => {
-  
-  return (
-    <div>
-      
-      <select  >
-        {currencyOptions.map(option => (
-          <option key={option.ID} value={option.CharCode}>{option.CharCode} {option.Name}</option>
-        ))}
-      </select>
-      <input  className="input"  />
-    </div>
-  )
-}
+const CurrencyRow = ({ currencyOptions, selectedCurrency , onChangeCurrency,amount,setAmount}: CurOptions) => {
+   return (
+      <div>
+         <select value={selectedCurrency} onChange={onChangeCurrency}>
+            {currencyOptions.map((option) => (
+               <option key={option.ID} value={option.CharCode}>
+                  {option.CharCode} {option.Name}
+               </option>
+            ))}
+         </select>
+         <input value={amount} onChange={setAmount} className="input" />
+      </div>
+   );
+};
 
-export default CurrencyRow
+export default CurrencyRow;
--- src/components/ItemExchange.tsx
+++ src/components/ItemExchange.tsx
@@ -5,16 +5,41 @@ import CurrencyRow from "./CurrencyRow";
 
 const ItemExchange = () => {
    const { curItems } = useTypedSelector((state) => state.items);
-   const [fromCurrency,setFromCurrency] = useState()
-   const [toCurrency,setToCurrency] = useState()
+   const [fromCurrency,setFromCurrency] = useState<string>("")
+   const [toCurrency,setToCurrency] = useState<string>("")
+   const [amount,setAmount] = useState()
+   const [amount2,setAmount2] = useState()
+   const onChangeFromCurrency = (e: React.ChangeEvent<HTMLInputElement>) =>{
+      setFromCurrency(e.target.value)
+   }
+
+   const onChangeToCurrency = (e: React.ChangeEvent<HTMLInputElement>) =>{
+      setToCurrency(e.target.value)
+   }
+
+  
+
+   console.log(amount)
+   console.log(amount2)
+
+   const onAmountValue = (e:any)=>{
+       setAmount(e.target.value)
+       const convertCurrencyInValue = curItems.filter(item => item === e.target.value)
+       const convertCurrencyToValue = curItems.filter(item => item === amount2)
+       console.log(convertCurrencyInValue, 'convertCurrencyInValue')
+   }
+      // setAmount(e.target.value)
+      
+   
+
    return (
       <div className="container">
          <div className="currency">
-            <CurrencyRow currencyOptions={curItems} />
+            <CurrencyRow currencyOptions={curItems} selectedCurrency={fromCurrency} onChangeCurrency={onChangeFromCurrency} amount={amount} setAmount={(e)=>setAmount(e.target.value)}/>
          </div>
          <button id="exchange">↕</button>
          <div className="currency">
-            <CurrencyRow currencyOptions={curItems} />
+            <CurrencyRow currencyOptions={curItems} selectedCurrency={toCurrency} onChangeCurrency={onChangeToCurrency} amount={amount2} setAmount={(e)=>setAmount2(e.target.value)}/>
          </div>
          <div className="result">
             <div className="rate" id="rate"></div>
